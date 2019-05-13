7be34ca781d66658a3641363f09eb22aaa3691dd
Dinko Ilicic <ilicicdinko@gmail.com>
2019-05-13 10:28:38 +0200

    Setup InstallSchema, registration.php and module.xml added

---
+++ Inchoo/Ticket/Setup/InstallSchema.php
@@ -0,0 +1,208 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inchoo
+ * Date: 5/13/19
+ * Time: 8:51 AM
+ */
+
+namespace Inchoo\Ticket\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     * @throws \Zend_Db_Exception
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $table = $setup->getConnection()->newTable(
+            $setup->getTable('inchoo_customer_ticket')
+        )->addColumn(
+            'ticket_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            [
+                'identity' => true,
+                'unsigned' => true,
+                'nullable' => false,
+                'primary' => true
+            ],
+            'Ticket ID'
+        )->addColumn(
+            'subject',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            ['nullable' => false],
+            'Subject'
+        )->addColumn(
+            'message',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            null,
+            ['nullable' => false],
+            'Message'
+        )->addColumn(
+            'customer_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            [
+                'nullable' => false,
+                'unsigned' => true
+            ],
+            'Customer ID'
+        )->addColumn(
+            'website_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            [
+                'nullable' => false,
+                'unsigned' => true
+            ],
+            'Website ID'
+        )->addColumn(
+            'created_at',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            [
+                'nullable' => false,
+                'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT
+            ],
+            'Created at'
+        )->addColumn(
+            'updated_at',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            [
+                'nullable' => false,
+                'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE
+            ],
+            'Updated at'
+        )->addColumn(
+            'status',
+            \Magento\Framework\DB\Ddl\Table::TYPE_BOOLEAN,
+            null,
+            [
+                'nullable' => false,
+                'default' => false
+            ],
+            'Status'
+        )->addForeignKey(
+            $setup->getFkName(
+                'inchoo_customer_ticket',
+                'customer_id',
+                'customer_entity',
+                'entity_id'
+            ),
+            'customer_id',
+            $setup->getTable('customer_entity'),
+            'entity_id',
+            \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+        )->addForeignKey(
+            $setup->getFkName(
+                'inchoo_customer_ticket',
+                'website_id',
+                'store_website',
+                'website_id'
+            ),
+            'website_id',
+            $setup->getTable('store_website'),
+            'website_id',
+            \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+        )->setComment(
+            'Customer Ticket'
+        );
+
+        $setup->getConnection()->createTable($table);
+
+        $reply = $setup->getConnection()->newTable(
+            $setup->getTable('inchoo_customer_ticket_reply')
+        )->addColumn(
+            'reply_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            [
+                'identity' => true,
+                'unsigned' => true,
+                'nullable' => false,
+                'primary' => true
+            ],
+            'Reply ID'
+        )->addColumn(
+            'message',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            null,
+            ['nullable' => false],
+            'Message'
+        )->addColumn(
+            'ticket_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            [
+                'nullable' => false,
+                'unsigned' => true
+            ],
+            'Ticket ID'
+        )->addColumn(
+            'admin_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            [
+                'nullable' => true,
+                'unsigned' => true
+            ],
+            'Admin ID'
+        )->addColumn(
+            'created_at',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            [
+                'nullable' => false,
+                'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT
+            ],
+            'Created at'
+        )->addColumn(
+            'updated_at',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            [
+                'nullable' => false,
+                'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE
+            ],
+            'Updated at'
+        )->addForeignKey(
+            $setup->getFkName(
+                'inchoo_customer_ticket_reply',
+                'ticket_id',
+                'inchoo_customer_ticket',
+                'ticket_id'
+            ),
+            'ticket_id',
+            $setup->getTable('inchoo_customer_ticket'),
+            'ticket_id',
+            \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+        )->addForeignKey(
+            $setup->getFkName(
+                'inchoo_customer_ticket_reply',
+                'admin_id',
+                'admin_user',
+                'user_id'
+            ),
+            'admin_id',
+            $setup->getTable('admin_user'),
+            'user_id',
+            \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+        )->setComment(
+            'Customer Ticket Reply'
+        );
+
+        $setup->getConnection()->createTable($reply);
+
+        $setup->endSetup();
+    }
+}
