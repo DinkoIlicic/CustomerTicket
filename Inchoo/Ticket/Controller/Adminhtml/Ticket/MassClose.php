0050af6ec07d36e1ba7c00caa59e7af5bf4f204f
Dinko Ilicic <ilicicdinko@gmail.com>
2019-05-15 11:54:51 +0200

    Added ui components for listing all tickets, added massclose and massdelete actions

---
+++ Inchoo/Ticket/Controller/Adminhtml/Ticket/MassClose.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inchoo
+ * Date: 5/15/19
+ * Time: 10:55 AM
+ */
+
+namespace Inchoo\Ticket\Controller\Adminhtml\Ticket;
+
+use Inchoo\Ticket\Api\TicketRepositoryInterface;
+use Magento\Backend\App\Action;
+
+class MassClose extends Action
+{
+    /**
+     * @var TicketRepositoryInterface
+     */
+    private $ticketRepository;
+
+    public function __construct(
+        Action\Context $context,
+        TicketRepositoryInterface $ticketRepository
+    ) {
+        parent::__construct($context);
+        $this->ticketRepository = $ticketRepository;
+    }
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|string
+     */
+    public function execute()
+    {
+        $message = null;
+        $data = $this->getRequest()->getParam('selected');
+        foreach ($data as $id) {
+            try {
+                $ticket = $this->ticketRepository->getById($id);
+                $ticket->setStatus(true);
+                $this->ticketRepository->save($ticket);
+            } catch (\Exception $e) {
+                return $message = $e->getMessage();
+            }
+        }
+
+        if ($message === true) {
+            $this->messageManager->addSuccessMessage('Tickets closed');
+        } elseif ($message !== null and $message !== true) {
+            $this->messageManager->addErrorMessage($message);
+        }
+
+        return $this->_redirect('ticket/ticket/');
+    }
+}
---
+++ Inchoo/Ticket/Ui/Component/Listing/DataProvider.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inchoo
+ * Date: 5/15/19
+ * Time: 10:22 AM
+ */
+
+namespace Inchoo\Ticket\Ui\Component\Listing;
+
+class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
+{
+    /**
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param \Inchoo\Ticket\Model\ResourceModel\Ticket\CollectionFactory $collectionFactory
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        \Inchoo\Ticket\Model\ResourceModel\Ticket\CollectionFactory $collectionFactory,
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+
+        $this->collection = $collectionFactory->create();
+    }
+
+    /**
+     * This class can be declared with virtualType
+     *
+     * {@inheritdoc}
+     */
+    public function getData()
+    {
+        $data = $this->getCollection()->toArray();
+        return $data;
+    }
+}
